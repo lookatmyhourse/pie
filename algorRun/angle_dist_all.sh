433c99dfd5326306aa0bb16eede96c6bbf20c7e2
mnost <balaz.kocis@gmail.com>
2016-04-14 22:53:52 +0200

    1

--- algorRun/angle_dist_all.sh
+++ algorRun/angle_dist_all.sh
@@ -12,12 +12,53 @@ file at the directory.
 The last step wil be to collect all .json files and to merge
 them to one DataFrame 
 
+run the script at the _refinements folder 
+
 END_TXT
 
+# to path to the root folder 
+starting_dir=`pwd`	
 
+# a function for folder change 
+# it takes an argument ! as:
+# to_cd arch_3_1_127_1
+#
+to_cd(){ cd "$1"; }   # when writing a bash function this is equal to
+to_cd(){
+cd "$1"
+
+angle_dist_.sh
+
+cd $starting_dir
+}
+
+
+
+# the step number is forwarded to this script 
+# by calling: 
+# angle_dist_all.sh 26 
 step_number=$1 
 
-# find all *dis files 
-find . -regextype posix-egrep -regex "(.*$step_number.*\.*.dis)"
+# find all *dis files for a certain step / step_folder
+f=`find . -regextype posix-egrep -regex "(.*$step_number.*\.*.dis)"`
+
+
+
+# navigate to the folder where the *.dis is 
+
+# execute 'angle_dist_.sh' that will generate the json file for that refinemets *.dis
+
+# get back to root folder 
+
+# repeat for all subfolders and all *dis files 
+
+# find all *.json files copy them to the root, open in python and merge them 
+
+
+
+
+
+
+
 
 
