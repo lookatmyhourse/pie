aeb9ac723847de456a6aee9873a5f27426ef2499
mnost <balaz.kocis@gmail.com>
2016-04-27 18:23:43 +0200

    1

---
+++ nja2dat_simplified/v_3.o/main_zu_multipleFiles.py
@@ -0,0 +1,117 @@
+# -*- coding: utf8 -*-
+import os,sys
+from pylab import *
+from decimal import *
+from scipy.interpolate import splrep #interpolate
+from scipy.interpolate import splev
+from operator import sub
+from scipy.optimize import curve_fit
+
+def dependencies_for_myprogram():
+    from scipy.sparse.csgraph import _validation
+
+	
+def nja2dat(fname):
+	#fixed values for the calculation
+	a1=Decimal(0.0484)
+	a2=Decimal(0.00084)
+	#stepsize
+	stepsize=0.013
+	#-------------------------
+	twotheta=[]
+	intensity=[]
+	twotheta_corr=[]
+	twotheta_corr_list=[]
+	twotheta_dat=[]
+	
+	f = open(fname, 'r')
+	lines=f.readlines()
+	f.close()
+
+	getcontext().prec = 5
+	for i in range(0,len(lines)):
+	#	twotheta.append(Decimal(lines[i][0:10]))
+		twotheta.append(Decimal(lines[i].split()[0]))
+	#	intensity.append(int(lines[i][11:-3])) 			 
+		intensity.append(int(lines[i].split()[1])) 			 
+		while twotheta[i]<Decimal('60.0000'):
+			twotheta_corr.append(Decimal(twotheta[i]-(a1-a2*twotheta[i])))
+			twotheta_dat.append(Decimal(twotheta[i]))
+			twotheta_corr_list.append(twotheta_corr[i])
+			break
+		else:
+			twotheta_corr=twotheta
+			twotheta_dat.append(Decimal(twotheta[i]))
+			twotheta_corr_list.append(twotheta_corr[i])
+
+	first_point=float(min(twotheta_corr_list))
+	last_point=float(max(twotheta_corr_list))
+	lent=len(twotheta_corr_list)
+	
+	xnew=arange(first_point,last_point,stepsize)
+	twotheta_corr_list_floats=[]
+	for i in range(0,lent):
+		ss=Decimal(twotheta_corr_list[i])
+		ss2=float(ss)
+		twotheta_corr_list_floats.append(ss2)
+	
+	#SPLINE interpolation / cubic spline
+	# tck=interpolate.splrep(self.twotheta_corr_list_floats, self.intensity,s=0)
+	# self.fl_spline=interpolate.splev(self.xnew,tck,der=0)
+	
+	tck=splrep(twotheta_corr_list_floats, intensity,s=0)
+	fl_spline=splev(xnew,tck,der=0)
+	
+	#creating the file of the interpolation 
+	newfilename2=fname[0:-4]+'_corr.dat'
+	f1=open(newfilename2,'w')
+	for i in range(0,lent):
+		f1.write('   ')
+		f1.write(str(round(xnew[i],4)))
+		f1.write('         ')
+		f1.write(str(round(fl_spline[i],0)))
+		f1.write('\n')
+	f1.close()
+
+#read uncorrected datafiles from the _data_files folder and work them through
+def apply_peak_pos_correction():
+	"""
+	Applies the peak shift correction for all the files in the _data_files 
+	folder. 
+	Execute at the current folder where the '_data_files' is a subfolder
+	"""
+	#start_time=time.strftime('%D %H:%M:%S')
+	#get the path of the current folder
+	mother_folder=os.getcwd()
+	
+	#list out the content of the data_files folder 
+	dat=os.listdir('./_data_files/')
+	#change the working folder to the _data_files
+	os.chdir('_data_files')
+	#
+	#c=0
+	for i in dat:
+	#	c=c+1
+		nja2dat(i)
+		#t='./_data_files/'+i
+		#shutil.copy(t,os.getcwd())
+#		run_this="algorRun.py "+pcr[0]+' '+i+' '+ctrl[0]
+#		a=subprocess.Popen(run_this,shell=True,stdout=None)
+#		if c==n:
+#			a.wait()
+#			c=0
+#	a.wait()
+#	[os.remove(i) for i in dat]
+	#b=time.strftime('%D %H:%M:%S')
+	#print 'start %s    end     %s' % (start_time,b)
+
+	#get back to the mother folder
+	os.chdir(mother_folder)
+
+
+if __name__ == "__main__":
+	apply_peak_pos_correction()	
+#    app = QtGui.QApplication(sys.argv)
+#    myapp = GUIForm()
+#    myapp.show()
+#    sys.exit(app.exec_())
